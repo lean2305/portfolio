d2d5d67760d882ead6bd67638f8aefe803d09788
lean2305 <lo0184975@gmail.com>
2023-11-22 21:46:20 +0000

    curriculo

--- curriculo/download.php
+++
@@ -1,8 +0,0 @@
-<?php
-$filename = 'Curriculo_Colorido_com_Gradiente_e_Fotografia_para_Designer_2.pdf';
-
-header('Content-type: application/pdf');
-header('Content-Disposition: attachment; filename="' . $filename . '"');
-readfile($filename);
-header("Location: ../index.html");
-?>
